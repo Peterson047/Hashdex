d89151695b0caa7cab2d29de309f9a3579ee3285
Peterson Alves <peterrson047@gmail.com>
2021-12-04 23:01:26 -0300

    correção de design e estruturação do código
    diminui o código javascript do html e coloquei em arquivos separados. fonte foi adicionada à alguns elementos e foi corrigido o logotipo que agora é transparente e um esboço mais fiel ao design final do projeto.

---
+++ assets/js/ibovespa.js
@@ -0,0 +1,16 @@
+new TradingView.widget(
+    {
+        "z-index": 1,
+        "autosize": true,
+        "symbol": "INDEX:IBOV",
+        "interval": "1",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "br",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_673aa"
+    }
+);
---
+++ assets/js/nasdaq.js
@@ -0,0 +1,15 @@
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "NASDAQ",
+        "interval": "1",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "br",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_f9060"
+    }
+);
