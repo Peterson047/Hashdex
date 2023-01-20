6a44c954a68e2de656835b38dab2299208fb227f
Peterson Alves <peterrson047@gmail.com>
2023-01-19 23:46:06 -0300

    reestruturando paginas de js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,38 @@ function openPage(pageName, elmnt, color) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+//NASDAQ
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "NASDAQ",
+        "interval": "1",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "br",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_f9060"
+    }
+);
+
+//IBOVESPA
+new TradingView.widget(
+    {
+        "z-index": 1,
+        "autosize": true,
+        "symbol": "INDEX:IBOV",
+        "interval": "1",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "br",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_673aa"
+    }
+);
