07418129d83ed6108618e77ad996e3d4978f7c03
Peterson Alves <peterrson047@gmail.com>
2024-01-06 15:49:39 -0300

    otimização
    movido uns scripts para o main.js, modificado o botao de ativo

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,12 +15,137 @@ function openPage(pageName, elmnt, color) {
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
+
+
+
+
+
+//SCRIPT OCULTA QUALQUER ITEM ABAIXO DA DIV "TABS" as
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtém a referência para a div com a classe "tabs"
+    var tabsDiv = document.querySelector('.tabs');
+
+    // Percorre os elementos abaixo da div "tabs"
+    var currentElement = tabsDiv.nextElementSibling;
+    while (currentElement) {
+        // Oculta o elemento
+        currentElement.style.display = 'none';
+
+        // Passa para o próximo elemento
+        currentElement = currentElement.nextElementSibling;
+    }
+});
+
+
+
+
+//SCRIPT BOTAO BUSCAR ATIVO
+function toggleInput() {
+    var input = document.getElementById("inputValor");
+    var btnOK = document.getElementById("btnOK");
+    var btnAtualizar = document.getElementById("btnAtualizar");
+
+    if (input.style.display === "none") {
+        input.style.display = "inline-block";
+        btnOK.style.display = "inline-block";
+        btnAtualizar.style.display = "none";
+    } else {
+        input.style.display = "none";
+        btnOK.style.display = "none";
+        btnAtualizar.style.display = "inline-block";
+    }
+}
+
+function confirmInput() {
+    var input = document.getElementById("inputValor");
+    var btnOK = document.getElementById("btnOK");
+    var btnAtualizar = document.getElementById("btnAtualizar");
+
+    input.style.display = "none";
+    btnOK.style.display = "none";
+    btnAtualizar.style.display = "inline-block";
+}
+
+function checkEnter(event) {
+    if (event.key === "Enter") {
+        confirmInput();
+    }
+}
+function toggleInput() {
+    var input = document.getElementById("inputValor");
+    var btnOK = document.getElementById("btnOK");
+    var btnAtualizar = document.getElementById("btnAtualizar");
+
+    if (input.style.display === "none") {
+        input.style.display = "inline-block";
+        btnOK.style.display = "inline-block";
+        btnAtualizar.style.display = "none";
+    } else {
+        input.style.display = "none";
+        btnOK.style.display = "none";
+        btnAtualizar.style.display = "inline-block";
+    }
+}
+
+function confirmInput() {
+    var input = document.getElementById("inputValor");
+    var btnOK = document.getElementById("btnOK");
+    var btnAtualizar = document.getElementById("btnAtualizar");
+
+    input.style.display = "none";
+    btnOK.style.display = "none";
+    btnAtualizar.style.display = "inline-block";
+}
+
+function checkEnter(event) {
+    if (event.key === "Enter") {
+        confirmInput();
+    }
+}
+
+
+
+
+// Obtém referências para os elementos do DOM
+var inputValor = document.getElementById('inputValor');
+var btnAtualizar = document.getElementById('btnAtualizar');
+var iframe = document.getElementById('tradingview_29c72-selector');
+
+// Adiciona um ouvinte de evento para a tecla Enter pressionada
+inputValor.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        atualizarIframe();
+    }
+});
+
+// Adiciona um ouvinte de evento para o botão "Atualizar" clicado
+btnAtualizar.addEventListener('click', function () {
+    atualizarIframe();
+});
+
+// Função para atualizar o atributo "src" do iframe
+function atualizarIframe() {
+    var novoValor = inputValor.value;
+
+    // Atualiza o atributo "src" do iframe com o novo valor
+    iframe.src = iframe.src.replace(/symbol=[^&]*/, 'symbol=' + novoValor);
+}
+
+
+
+
+
+
+
+
 //NASDAQ
 new TradingView.widget(
     {
         "autosize": true,
         "symbol": "NASDAQ",
-        "interval": "1",
+        "interval": "D",
         "timezone": "Etc/UTC",
         "theme": "dark",
         "style": "1",
@@ -38,7 +163,7 @@ new TradingView.widget(
         "z-index": 1,
         "autosize": true,
         "symbol": "INDEX:IBOV",
-        "interval": "1",
+        "interval": "D",
         "timezone": "Etc/UTC",
         "theme": "dark",
         "style": "1",
@@ -55,9 +180,9 @@ new TradingView.widget({
     "width": "100%",
     "height": "450px",
     "symbol": get_param("symbol", get_param("tvwidgetsymbol", "IBOV")),
-    "interval": "1",
+    "interval": "D",
     "timezone": "exchange",
-    "theme": "White",
+    "theme": "dark",
     "style": "3",
     "toolbar_bg": "#f1f3f6",
     "withdateranges": true,
@@ -73,6 +198,10 @@ new TradingView.widget({
     "customer": "BMFBOVESPA:XPML11",
     "locale": get_lang() == 'pt' ? 'br' : get_lang()
 });
+
+
+
+
 //SCRIPT INPUT CODIGO INDICE
 // Obtém referências para os elementos do DOM
 var inputValor = document.getElementById('inputValor');
